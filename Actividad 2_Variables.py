5039c1754cd20b5956829100dd90145b7d8dd116
ArielConstante03 <freefrc3@gmail.com>
2026-04-10 08:17:14 -0500

    8 17 10 04 2026

--- Actividad 2_Variables.py
+++ Actividad 2_Variables.py
@@ -2,10 +2,12 @@
 # 08 04 2026
 #Actividad 2
     #Ejercicio 1
-first_name, last_name, country, age, is_married = "Ariel", "Constante", "Ecuador", 17, False
+first_name, last_name, country, age, is_married, is_true, light_on = "Ariel", "Constante", "Ecuador", 17, False, False, True
 print(first_name, last_name, country, age, is_married)
 print("First Name:", first_name)
 print("Last Name:", last_name)
 print("Country:", country)
 print("Age:", age)
 print("Married:", is_married)
+print("Is True:", is_true)
+print("Light On:", light_on)
