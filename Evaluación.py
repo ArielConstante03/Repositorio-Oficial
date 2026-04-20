02df9b462ad358b70e298ec8b3e0a4911855b7f7
ArielConstante03 <freefrc3@gmail.com>
2026-04-20 09:28:30 -0500

    Evaluación

---
+++ Evaluación.py
@@ -0,0 +1,23 @@
+#Evaluación 1
+# ===== PARTE A =====
+# 1. Análisis de datos y código
+nombre = "Lucía"
+edad = 16
+promedio = 9.75
+cursos = ["Python", "HTML", "CSS"]
+print(type(nombre))
+print(type(edad))
+print(type(promedio))
+print(type(cursos))
+print(len(nombre))
+
+# Respuesta 2:
+
+# ===== PARTE B =====
+
+# Código corregido
+
+# ===== PARTE C =====
+
+# Programa integrador
+
