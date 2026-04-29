dd0be33c24110fe6e2aae5af71c3d0e1da55e27a
ArielConstante03 <freefrc3@gmail.com>
2026-04-29 12:02:51 -0500

    Nuevo archivo

---
+++ Desafío Python.py
@@ -0,0 +1,11 @@
+#Ariel Constante
+#29 04 2026
+#Desafío 1
+#%%
+nombre = input()
+edad = int(input())
+puntaje = int(input())
+asistencia = int(input())
+codigo_invitacion = int(input())
+print(nombre.upper)
+# %%
